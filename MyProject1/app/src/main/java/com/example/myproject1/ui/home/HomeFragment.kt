340aa3d356003b52055bad9174757d3c44d9ad6f
NickCoder22 <svzin233@yandex.ru>
2022-05-27 14:47:16 +0300

    init

---
+++ MyProject1/app/src/main/java/com/example/myproject1/ui/home/HomeFragment.kt
@@ -0,0 +1,21 @@
+package com.example.myproject1.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.myproject1.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+
+}
